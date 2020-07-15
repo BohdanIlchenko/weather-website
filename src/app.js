22d3101f6b999bd5da7bc0f763cf8728da482fac
bohdan.il4enko <bohdan.il4enko@gmail.com>
2020-07-15 17:59:09 +0300

    heroku config

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import geocode from './utils/geocode.js'
 import forecast from './utils/forecast.js'
 
 const app = express()
+const port = process.env.PORT || 3000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 })
